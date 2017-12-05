15b1e2611d77c6f6535b4108ebb6397ad23d92a3
Aleksey-Kolyasa-dev <aleksey.kolyasa.dev@gmail.com>
2017-12-05 18:40:25 +0200

    -- hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { landingModule } from "./landing/landing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    landingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
