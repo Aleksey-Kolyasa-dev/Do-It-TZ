d016a95fd9b16b7fffc58557fb216153bdd8e893
Aleksey-Kolyasa-dev <aleksey.kolyasa.dev@gmail.com>
2017-12-06 12:52:41 +0200

    -s

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CarouselComponent, SocialsComponent, NavComponent, HeroComponent } from './index';
+import { CarouselComponent, SocialsComponent, NavComponent, HeroComponent, AccordionComponent } from './index';
 
 
-let list: any[] = [ CarouselComponent, SocialsComponent, NavComponent, HeroComponent ];
+let list: any[] = [ CarouselComponent, SocialsComponent, NavComponent, HeroComponent, AccordionComponent ];
 
 @NgModule({
   imports: [ CommonModule ],
-  declarations: [ list, HeroComponent ],
+  declarations: [ list ],
   exports: [ list ]
 })
 
